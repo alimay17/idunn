435139a1c1f827f95d3a8e5f55f9bbec65c6ef62
Aly Cortenoever <alimay.smith@outlook.com>
2022-01-03 17:52:43 -0600

    added story editor example

---
+++ week01/storyEditor.js
@@ -0,0 +1,29 @@
+function loadStory() {
+  const storyName = document.getElementById('nameInput').value;
+  const storyHTML = localStorage.getItem(storyName);
+  const storyEditor = document.getElementById('storyEditor');
+  
+  storyEditor.value = storyHTML;
+}
+
+function saveStory() {
+  const storyName = document.getElementById('nameInput').value;
+  const storyHTML = document.getElementById('storyEditor').value;
+  
+  localStorage.setItem(storyName, storyHTML);
+}
+
+function displayStory() {
+  const storyHTML = document.getElementById('storyEditor').value;
+  const storyDisplay = document.getElementById('storyDisplay');
+
+  storyDisplay.innerHTML = storyHTML;
+}
+
+const loadButton = document.getElementById('load');
+const saveButton = document.getElementById('save');
+const displayButton = document.getElementById('display');
+
+loadButton.addEventListener('click', loadStory);
+saveButton.addEventListener('click', saveStory);
+displayButton.addEventListener('click', displayStory);
