e144f034ab9ea5711d004dc2b792b0d9e0dabe58
Aly Smith <alimay.smith@outlook.com>
2022-01-11 12:50:28 -0600

    initial heroes file

---
+++ week02/heroes.js
@@ -0,0 +1,21 @@
+/*============================================
+=  Quiz Data for SuperHero Quiz - Week 02    =
+= Author: Alice Smith                        =
+=============================================*/
+export const heroes = [{
+  name: "Superman",
+  realName: "Clark Kent"
+},
+{
+  name: "Wonder Woman",
+  realName: "Diana Prince"
+},
+{
+  name: "Batman",
+  realName: "Bruce Wayne"
+},
+{
+  name: "Tarzan",
+  realName: "John Clayton"
+}
+]
