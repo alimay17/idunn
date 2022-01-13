10420603e231008ceff5887aeef6e5c4caae03b8
Aly Smith <alimay.smith@outlook.com>
2022-01-13 14:55:43 -0600

    finished mobile nav ready for validation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 = Author: Alice Smith                         =
 =============================================*/
 
-import {navLinks} from './navlinks.js';
+import { navLinks } from './navlinks.js';
 /* Create a dynamic menu from array data */
 function buildMenu(myMenu) {
   const navDisplay = document.getElementById('myNav');
@@ -12,15 +12,12 @@ function buildMenu(myMenu) {
     aLink.href = element.URL;
     aLink.id = element.ID;
     aLink.textContent = element.Label;
-    
 
     let aListItem = document.createElement('li');
     aListItem.appendChild(aLink);
 
     navDisplay.appendChild(aListItem);
   });
-  let active = document.getElementById('active');
-  console.log(active);
 }
 
 buildMenu(navLinks);
