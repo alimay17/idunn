6c34acc85b63d5a109054b699d065487ce22504f
Aly Smith <alimay.smith@outlook.com>
2022-01-19 15:52:23 -0600

    cleaned up code, ready for validation

---
+++ week03/js/helpers.js
@@ -0,0 +1,66 @@
+/* Hero Helpers object */
+
+export const heroHelpers = {
+  getHeroes(element, heroes) {
+    const myDisplay = element;
+    myDisplay.innerHTML = '';
+    for (const hero of heroes) {
+      let myHero = document.createElement('li');
+      myHero.innerText = `${hero.alias} - Real Name: ${hero.realName}`;
+      myDisplay.appendChild(myHero);
+    }
+  },
+
+  sortHeroesAlias(heroes) {
+    return heroes.sort((a, b) => {
+      let aName = a.alias.toLowerCase();
+      let bName = b.alias.toLowerCase();
+      if (aName < bName) {
+        return -1;
+      } else if (aName > bName) {
+        return 1;
+      } else return 0;
+    });
+  },
+
+  sortHeroesName(heroes) {
+    return heroes.sort((a, b) => {
+      let aName = a.realName.toLowerCase().split(' ');
+      let bName = b.realName.toLowerCase().split(' ');
+      if (aName[1] < bName[1]) {
+        return -1;
+      } else if (aName[1] > bName[1]) {
+        return 1;
+      } else return 0;
+    });
+  }
+}
+
+
+/* View and Render helpers Object */
+export const view = {
+  score: document.getElementById('score'),
+  info: document.getElementById('info'),
+  result: document.getElementById('result'),
+
+  show(element) {
+    element.style.display = 'block';
+  },
+
+  hide(element) {
+    element.style.display = 'none';
+  },
+
+  toggle(element) {
+    element.style.display === "block" ? element.style.display = "none" :
+    element.style.display = "block";
+  },
+
+  getElement(string) {
+    return document.getElementById(string);
+  },
+
+  render(target, content) {
+    target.innerHTML = content;
+  }
+}
--- week03/js/main.js
+++ week03/js/main.js
@@ -3,103 +3,53 @@
 = Author: Alice Smith                        =
 =============================================*/
 
-/* Heroes Object */
-const heroes = [{
-    alias: "Superman",
-    realName: "Clark Kent"
-  },
-  {
-    alias: "Wonder Woman",
-    realName: "Diana Prince"
-  },
-  {
-    alias: "Batman",
-    realName: "Bruce Wayne"
-  },
-  {
-    alias: "Tarzan",
-    realName: "John Clayton"
-  }
-]
+import { heroHelpers, view } from "./helpers.js";
+import { heroes } from "./heroes.js";
+import { game} from "./game.js";
 
-/* My Heroes namespace object */
-const heroHelpers = {
-  show(element) {
-    element.style.display = 'block';
-  },
-  hide(element) {
-    element.style.display = 'none';
-  },
-  toggle(element) {
-    element.style.display === "block" ? element.style.display = "none" :
-      element.style.display = "block";
-  },
-
-  getElement(string) {
-    return document.getElementById(string);
-  },
-
-  getHeroes(element, heroes) {
-    const myDisplay = element;
-    myDisplay.innerHTML = '';
-    for (const hero of heroes) {
-      let myHero = document.createElement('li');
-      myHero.innerText = `${hero.alias} - Real Name: ${hero.realName}`;
-      myDisplay.appendChild(myHero);
-    }
-  },
-
-  sortHeroesAlias(heroes) {
-    return heroes.sort((a, b) => {
-      let aName = a.alias.toLowerCase();
-      let bName = b.alias.toLowerCase();
-      if ( aName < bName) {
-        return -1;
-      } else if (aName > bName) {
-        return 1;
-      } else return 0;
-    });
-  }, 
-  sortHeroesName(heroes){
-    return heroes.sort((a, b) => {
-      let aName = a.realName.toLowerCase().split(' ');
-      let bName = b.realName.toLowerCase().split(' ');
-      if ( aName[1] < bName[1]) {
-        return -1;
-      } else if (aName[1] > bName[1]) {
-        return 1;
-      } else return 0;
-    });
-  }
-}
-
-// html element variables
-const heroDisplay = heroHelpers.getElement('heroDisplay');
-const toggleButton = heroHelpers.getElement('toggle');
-const sort1Button = heroHelpers.getElement('sort1');
-const sort2Button = heroHelpers.getElement('sort2');
-const reset = heroHelpers.getElement('reset');
+// DOM Element variables
+const heroDisplay = view.getElement('heroDisplay');
+const toggleButton = view.getElement('toggle');
+const sort1Button = view.getElement('sort1');
+const sort2Button = view.getElement('sort2');
+const resetButton = view.getElement('reset');
+const scoreButton = view.getElement('scoreButton');
+const quizButton = view.getElement('quiz');
 
 // initial state
-heroHelpers.hide(heroDisplay);
 heroHelpers.getHeroes(heroDisplay, heroes);
+view.show(heroDisplay);
 
 // event listeners
 toggleButton.addEventListener('click', () => {
-  heroHelpers.toggle(heroDisplay);
+  view.toggle(heroDisplay);
 });
 
 sort1Button.addEventListener('click', () => {
   const sorted = heroHelpers.sortHeroesAlias(heroes);
   heroHelpers.getHeroes(heroDisplay, sorted);
-  heroHelpers.show(heroDisplay);
+  view.show(heroDisplay);
 });
 
 sort2Button.addEventListener('click', () => {
   const sorted = heroHelpers.sortHeroesName(heroes);
   heroHelpers.getHeroes(heroDisplay, sorted);
-  heroHelpers.show(heroDisplay);
+  view.show(heroDisplay);
 });
 
-reset.addEventListener('click', ()=>{
-window.location.reload();})
+reset.addEventListener('click', () => {
+  localStorage.clear();
+  view.hide(view.info);
+});
+
+quizButton.addEventListener('click', ()=>{
+  view.hide(heroDisplay);
+  setTimeout(()=>game.start(heroes), 1);
+});
+
+scoreButton.addEventListener('click', ()=>{
+  game.getScore();
+  view.show(view.info);
+});
+
+
