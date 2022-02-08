d75ddaecdce4ca8aff5a3735d0f3b8d8c5040a2b
Aly Smith <alimay.smith@outlook.com>
2022-02-08 15:02:24 -0600

    restructuring

---
+++ todoApp/src/todo.js
@@ -0,0 +1,10 @@
+/*============================================
+=            ToDo Class: TODO APP            =
+= Author: Alice Smith                        = 
+=============================================*/
+
+class Todo {
+  constructor(name) {
+    this.name = name;
+  }
+}
