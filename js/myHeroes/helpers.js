993cc5a9f8db04073f7b2a1eb0bd953d3dbd275f
Aly Smith <alimay.smith@outlook.com>
2022-09-25 12:22:47 -0600

    week03 done

--- js/myHeroes/helpers.js
+++ js/myHeroes/helpers.js
@@ -1,2 +1,92 @@
 /* Hero Helpers object */
+export const heroHelpers = {
+  getHeroes(element, heroes) {
+    const myDisplay = element;
 
+    // initial table setup
+    myDisplay.innerHTML = '';
+    let table = document.createElement('tr');
+    let header1 = document.createElement('th');
+    let header2 = document.createElement('th');
+    header1.innerText = 'Real Name';
+    table.appendChild(header1);
+    header2.innerText = 'Alias';
+    table.appendChild(header2);
+    myDisplay.appendChild(table);
+
+    // add hero data
+    for (const hero of heroes) {
+      let myHero = document.createElement('tr');
+      let alias = document.createElement('td');
+      let realName = document.createElement('td');
+      alias.innerText = hero.alias;
+      realName.innerText = hero.realName;
+      myHero.appendChild(realName);
+      myHero.appendChild(alias);
+      myDisplay.appendChild(myHero);
+    }
+  },
+
+  sortHeroesAlias(heroes) {
+    return heroes.sort((a, b) => {
+      let aName = a.alias.toLowerCase();
+      let bName = b.alias.toLowerCase();
+      if (aName < bName) {
+        return -1;
+      } else if (aName > bName) {
+        return 1;
+      } else return 0;
+    });
+  },
+
+  sortHeroesName(heroes) {
+    return heroes.sort((a, b) => {
+      let aName = a.realName.toLowerCase().split(' ');
+      let bName = b.realName.toLowerCase().split(' ');
+      if (aName[1] < bName[1]) {
+        return -1;
+      } else if (aName[1] > bName[1]) {
+        return 1;
+      } else return 0;
+    });
+  }
+}
+
+/* View and Render helpers object */
+export const view = {
+  info: document.getElementById('info'),
+  result: document.getElementById('result'),
+
+  show(element) {
+
+    if (element.nodeName == 'TABLE') {
+      element.style.display = 'table';
+    } else {
+      element.style.display = 'block';
+    }
+  },
+
+  hide(element) {
+    element.style.display = 'none';
+  },
+
+  toggle(element) {
+    if (element.style.display === 'block') {
+      element.style.display = 'none'
+    } else if (element.style.display === 'table') {
+      element.style.display = 'none'
+    } else if (element.nodeName == 'TABLE') {
+      element.style.display = 'table';
+    } else {
+      element.style.display = 'block';
+    }
+  },
+
+  getElement(string) {
+    return document.getElementById(string);
+  },
+
+  render(target, content) {
+    target.innerHTML = content;
+  }
+}
--- js/myHeroes/myHeroes.js
+++ js/myHeroes/myHeroes.js
@@ -3,5 +3,57 @@
 = Author: Alice Smith                        =
 =============================================*/
 
+/* Import Modules */
+import { heroHelpers, view } from "./helpers.js";
+import { heroes } from "./heroes.js";
+import { game} from "./game.js";
+
+/* DOM Element Variables */
+const heroDisplay = view.getElement('heroDisplay');
+const toggleButton = view.getElement('toggle');
+const sort1Button = view.getElement('sort1');
+const sort2Button = view.getElement('sort2');
+const resetButton = view.getElement('reset');
+const scoreButton = view.getElement('scoreButton');
+const quizButton = view.getElement('quiz');
+
+/* Initial State */
+heroHelpers.getHeroes(heroDisplay, heroes);
+view.show(heroDisplay);
+view.hide(result);
+view.hide(info);
+/* Event Listeners */
+toggleButton.addEventListener('click', () => {
+  view.toggle(heroDisplay);
+});
+
+sort1Button.addEventListener('click', () => {
+  const sorted = heroHelpers.sortHeroesAlias(heroes);
+  heroHelpers.getHeroes(heroDisplay, sorted);
+  view.show(heroDisplay);
+});
+
+sort2Button.addEventListener('click', () => {
+  const sorted = heroHelpers.sortHeroesName(heroes);
+  heroHelpers.getHeroes(heroDisplay, sorted);
+  view.show(heroDisplay);
+});
+
+resetButton.addEventListener('click', () => {
+  localStorage.clear();
+  view.hide(view.result);
+});
+
+quizButton.addEventListener('click', ()=>{
+  view.hide(heroDisplay);
+  view.hide(view.result);
+  setTimeout(()=>game.start(heroes), 1);
+});
+
+scoreButton.addEventListener('click', ()=>{
+  game.getScore();
+  view.show(view.result);
+});
+
 
 
