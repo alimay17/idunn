4f493cd052331f8a49bfb1c8c501ee09508faeea
Aly Smith <alimay.smith@outlook.com>
2022-10-05 16:54:13 -0600

    bug fixing and finishing

--- js/forms.js
+++ js/forms.js
@@ -3,16 +3,26 @@
 = Author: Alice Smith                       =
 =============================================*/
 
+/*----------  Helpers  ----------*/
+function show(element) {
+    element.style.display = 'block';
+}
+
+function hide(element) {
+  element.style.display = 'none';
+}
+
 /*----------  Basic Search Form  ----------*/
 const searchForm = document.forms['search'];
 const [input, button] = searchForm.elements;
 
 function search(event) {
-  document.getElementById('searchResult').innerText = `You searched for: "${input.value}"`;
+  let sDisplay = document.getElementById('searchResult');
+  show(sDisplay);
+  sDisplay.innerText = `You searched for: "${input.value}"`;
   event.preventDefault();
 }
 searchForm.addEventListener('submit', search, false);
-
 
 /*----------  Make New Hero  ----------*/
 const newHeroForm = document.forms['hero'];
@@ -23,11 +33,11 @@ function makeHero(event) {
   hero.name = newHeroForm.heroName.value;
   hero.realName = newHeroForm.realName.value;
   let display = document.getElementById('showHero');
+  show(display);
   display.innerText = `Real Name: ${hero.realName}, Hero Name: ${hero.name}`;
   return hero;
 }
 newHeroForm.addEventListener('submit', makeHero, false);
-
 
 /*----------  Basic Form Practice  ----------*/
 const myForm = document.forms['example'];
@@ -52,12 +62,13 @@ function processForms(event) {
   formData.range = myForm.range.value;
   formData.hidden = myForm.hidden.value;
 
-  // display
   display(formData);
 }
 
+// display forms data
 function display(data) {
   const display = document.getElementById('output');
+  show(display);
   display.innerHTML = '';
   const ul = document.createElement('ul');
   for (const key in data) {
@@ -69,3 +80,8 @@ function display(data) {
 }
 
 myForm.addEventListener('submit', processForms, false);
+
+// initial page setup
+hide(document.getElementById('searchResult'));
+hide(document.getElementById('showHero'));
+hide(document.getElementById('output'));
