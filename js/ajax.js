0c182c9d5cf5a95be32c60de93c6f18c98aabd53
Aly Smith <alimay.smith@outlook.com>
2022-10-30 10:38:45 -0600

    ajax code examples added

---
+++ js/ajax.js
@@ -0,0 +1,40 @@
+/*============================================
+=         JS for AJAX - Week 07              =
+= Original Author: Darren Jones              =
+= Customized: Alice Smith                    =
+=============================================*/
+
+const textButton = document.getElementById('number');
+const apiButton = document.getElementById('chuck');
+const outputDiv = document.getElementById('output');
+
+const textURL = 'http://numbersapi.com/random';
+const apiURL = 'https://api.chucknorris.io/jokes/random';
+
+textButton.addEventListener('click', () => {
+  fetch(textURL)
+  .then( response => {
+      outputDiv.innerHTML = 'Waiting for response...';
+  if(response.ok) {
+      return response;
+  }
+      throw Error(response.statusText);
+  })
+  .then( response => response.text() )
+  .then( text => outputDiv.innerText = text )
+  .catch( error => console.log('There was an error:', error))
+},false);
+
+apiButton.addEventListener('click', () => {
+  fetch(apiURL)
+  .then( response => {
+      outputDiv.innerHTML = 'Waiting for response...';
+  if(response.ok) {
+      return response;
+  }
+  throw Error(response.statusText);
+  })
+  .then( response => response.json() )
+  .then( data => outputDiv.innerText = data.value )
+  .catch( error => console.log('There was an error:', error))
+},false);
